3dbf788577e0ef38d264f9e7099e7a6b9bc82844
alberto <albertoesbueno@hotmail.com>
2018-06-23 16:19:29 +0100

    MontFerrerDay
    And cdecountry exeggcutor

---
+++ Assets/Scripts/Utils/EnableOnDate.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class EnableOnDate : MonoBehaviour {
+
+	public int day;
+	public Month month;
+
+	public enum Month{
+		January = 0,
+		February = 1,
+		March = 2,
+		April = 3,
+		May = 4,
+		June = 5,
+		July = 6,
+		August = 7,
+		September = 8,
+		October = 9,
+		November = 10,
+		December = 11
+	};
+
+
+	void Start () {
+		var date = DateTime.Now.ToLocalTime();
+		bool isDate = date.Day == day && date.Month == (int)month;
+		gameObject.SetActive(isDate);
+	}
+	
+}
