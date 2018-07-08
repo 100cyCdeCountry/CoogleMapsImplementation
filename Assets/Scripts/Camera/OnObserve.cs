27956b6aa8ab401cb77002b4f85795758f51d64f
alberto <albertoesbueno@hotmail.com>
2018-07-08 16:27:05 +0100

    CdeCucarachas Update

--- Assets/Scripts/Camera/OnObserve.cs
+++ Assets/Scripts/Camera/OnObserve.cs
@@ -8,6 +8,7 @@ public class OnObserve : MonoBehaviour {
 	public UnityEvent onObserveEvent;
 	public bool onlyFirst = true;
 	public float minDistance = Mathf.Infinity;
+	[Range(0, 1)] public float probability = 1.0f;
 
 	bool observed = false;
 
@@ -20,14 +21,20 @@ public class OnObserve : MonoBehaviour {
 	{
 		if(Camera.main == null) return;
 
+		if(ShouldInvoke()) {
+			if(Random.Range(0.0f, 1.0f) <= probability)
+				onObserveEvent.Invoke();
+			observed = true;
+		}
+	}
+
+	bool ShouldInvoke() {
 		Vector3 screenPoint = Camera.main.WorldToViewportPoint(transform.position);
  		bool onScreen = screenPoint.z > 0 && screenPoint.x > 0 &&
 		  screenPoint.x < 1 && screenPoint.y > 0 && screenPoint.y < 1;
-		if(onScreen && (!onlyFirst || !observed) 
-			&& Vector3.Distance(Camera.main.transform.position, transform.position) < minDistance) {
-			onObserveEvent.Invoke();
-			observed = true;
-		}
+
+		return onScreen && (!onlyFirst || !observed) 
+			&& Vector3.Distance(Camera.main.transform.position, transform.position) < minDistance;
 	}
 
 	public void DebugMsg(string s) {
